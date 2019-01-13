9d1ed736945ee23a47b456845a7a38f3f4f3cabb
Maciej Sikorski <maciej.sikorski@valueadd.pl>
2019-01-13 17:25:54 +0100

    Added ices to connection.config

--- src/configs/connection.config.sample.ts
+++ src/configs/connection.config.sample.ts
@@ -1,18 +1,22 @@
 import * as path from 'path';
 
 export const connectionConfig = {
-  youtube: {
-    apiKey: 'xxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-  },
-  slack: {
-    token: 'xoxb-xxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxx'
+  ices: {
+    host: 'ices',
+    port: 8888
   },
   redis: {
     host: 'redis',
     port: 6379
   },
+  slack: {
+    token: 'xoxb-xxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxx'
+  },
   tracks: {
     directory: path.join(__dirname, 'public', 'tracks'),
     normalizationDb: 92
+  },
+  youtube: {
+    apiKey: 'xxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
   }
 };
