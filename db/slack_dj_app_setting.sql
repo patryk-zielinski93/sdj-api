e2407ba3f729a8213015c0d8cbf68c93469c9c81
Maciej Sikorski <maciej.sikorski@valueadd.pl>
2019-01-18 01:20:37 +0100

    removed sample db sql

--- db/slack_dj_app_setting.sql
+++
@@ -1,8 +0,0 @@
-create table app_setting
-(
-  id    int auto_increment
-    primary key,
-  type  varchar(255) not null,
-  value varchar(255) not null
-);
-
--- db/slack_dj_bot_response.sql
+++
@@ -1,8 +0,0 @@
-create table bot_response
-(
-  id      int auto_increment
-    primary key,
-  message varchar(255) not null,
-  type    varchar(255) not null
-);
-
