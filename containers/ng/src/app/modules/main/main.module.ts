8a76e67f13be5881ddbeb0ea87210bbc8f513984
bociuch <kontakt@dominikgasior.pl>
2019-02-17 17:29:14 +0100

    Basic project structure

--- containers/ng/src/app/modules/main/main.module.ts
+++ containers/ng/src/app/modules/main/main.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
-import { UserProfileComponent } from './navbar/user-profile/user-profile.component';
-import { MiniPlayerComponent } from './navbar/mini-player/mini-player.component';
+import { UserProfileComponent } from './components/navbar/user-profile/user-profile.component';
+import { MiniPlayerComponent } from './components/navbar/mini-player/mini-player.component';
 
 @NgModule({
   declarations: [
