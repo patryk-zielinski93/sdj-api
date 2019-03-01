98bf47aa8360db3b47e2662846b02b96a9c8dba5
Maciej Sikorski <maciej.sikorski@valueadd.pl>
2019-03-01 15:08:59 +0100

    Current track in awesome player

--- containers/ng/src/app/app.component.ts
+++ containers/ng/src/app/app.component.ts
@@ -1,84 +1,92 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { environment } from '@environment/environment';
-import { Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { filter, map, tap } from 'rxjs/operators';
 import { appConfig } from '../configs/app.config';
 import { QueuedTrack } from './common/interfaces/queued-track.interface';
 import { SpeechService } from './modules/core/services/speech.service';
 import { WebSocketService } from './modules/core/services/web-socket.service';
 
 @Component({
-  selector: 'sdj-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'sdj-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements AfterViewInit {
-  audioSrc = environment.radioStreamUrl;
-  dj: HTMLAudioElement;
-  queuedTracks$: Subject<QueuedTrack[]>;
+    audioSrc = environment.radioStreamUrl;
+    dj: HTMLAudioElement;
+    queuedTracks$: Subject<QueuedTrack[]>;
+    prvTrackId: number;
+    currentTrack: Observable<any>;
 
-  constructor(private ws: WebSocketService, private speechService: SpeechService) {
+    constructor(private ws: WebSocketService, private speechService: SpeechService) {
+    }
 
-  }
+    ngAfterViewInit(): void {
+        this.dj = <HTMLAudioElement>document.getElementById('dj');
 
-  ngAfterViewInit(): void {
-    this.dj = <HTMLAudioElement>document.getElementById('dj');
+        this.handleSpeeching();
+        this.handleWsEvents();
+    }
 
-    this.handleSpeeching();
-    this.handleWsEvents();
-  }
+    handleQueuedTrackList(): void {
+        this.queuedTracks$ = this.ws.getQueuedTrackListSubject();
+        this.queuedTracks$.subscribe((list) => {
+            console.log(list);
+        });
+        this.queuedTracks$.next();
 
-  handleQueuedTrackList(): void {
-    this.queuedTracks$ = this.ws.getQueuedTrackListSubject();
-    this.queuedTracks$.subscribe((list) => {
-      console.log(list);
-    });
-    this.queuedTracks$.next();
-  }
+        this.currentTrack = this.queuedTracks$
+            .pipe(map((list) => list[0]),
+                filter((track: any) => track && track.id !== this.prvTrackId),
+                tap((track: any) => this.prvTrackId = track.id)
+            );
+    }
 
-  handleSpeeching(): void {
-    this.speechService.startListening();
-    this.speechService.speeching.subscribe(
-      (speeching: boolean) => {
-        if (speeching) {
-          this.dj.volume = 0.1;
-        } else {
-          this.dj.volume = 1;
-        }
-      }
-    );
-  }
+    handleSpeeching(): void {
+        this.speechService.startListening();
+        this.speechService.speeching.subscribe(
+            (speeching: boolean) => {
+                if (speeching) {
+                    this.dj.volume = 0.1;
+                } else {
+                    this.dj.volume = 1;
+                }
+            }
+        );
+    }
 
-  handleWsEvents(): void {
-    const connect$ = this.ws.createSubject('connect');
-    const events$ = this.ws.createSubject('events');
-    connect$.subscribe(() => {
-      console.log('Connected');
-      events$.next(<any>{ test: 'test' });
-      this.handleQueuedTrackList();
-    });
-    events$.subscribe((data) => console.log('event', data));
-    const disconnect$ = this.ws.createSubject('disconnect');
-    disconnect$.subscribe(() => console.log('Disconnected'));
-    const exception$ = this.ws.createSubject('exception');
-    exception$.subscribe(() => console.log('Disconnected'));
+    handleWsEvents(): void {
+        const connect$ = this.ws.createSubject('connect');
+        const events$ = this.ws.createSubject('events');
+        connect$.subscribe(() => {
+            console.log('Connected');
+            events$.next(<any>{ test: 'test' });
+            this.handleQueuedTrackList();
+        });
+        events$.subscribe((data) => console.log('event', data));
+        const disconnect$ = this.ws.createSubject('disconnect');
+        disconnect$.subscribe(() => console.log('Disconnected'));
+        const exception$ = this.ws.createSubject('exception');
+        exception$.subscribe(() => console.log('Disconnected'));
 
-    const playDJ$ = this.ws.createSubject('play_dj');
-    playDJ$.subscribe((data) => {
-      console.log('dj');
-      if (this.audioSrc !== environment.radioStreamUrl) {
-        this.dj.load();
-      }
-      this.dj.play();
-      this.audioSrc = environment.radioStreamUrl;
-    });
-    const playRadio$ = this.ws.createSubject('play_radio');
-    playRadio$.subscribe(() => {
-      console.log('radio');
-      if (this.audioSrc !== appConfig.externalStream) {
-        this.dj.load();
-      }
-      this.dj.play();
-      this.audioSrc = appConfig.externalStream;
-    });
-  }
+        const playDJ$ = this.ws.createSubject('play_dj');
+        playDJ$.subscribe((data) => {
+            console.log('dj');
+            if (this.audioSrc !== environment.radioStreamUrl) {
+                this.dj.load();
+            }
+            this.dj.play();
+            this.audioSrc = environment.radioStreamUrl;
+        });
+        const playRadio$ = this.ws.createSubject('play_radio');
+        playRadio$.subscribe(() => {
+            console.log('radio');
+            if (this.audioSrc !== appConfig.externalStream) {
+                this.dj.load();
+            }
+            this.dj.play();
+            this.audioSrc = appConfig.externalStream;
+        });
+    }
 }
--- containers/ng/src/app/modules/main/main.module.ts
+++ containers/ng/src/app/modules/main/main.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { AwesomePlayerComponent } from './components/awsome-player/awesome-player.component';
+import { AwesomePlayerComponent } from './components/awesome-player/awesome-player.component';
 import { MiniPlayerComponent } from './components/navbar/mini-player/mini-player.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { UserProfileComponent } from './components/navbar/user-profile/user-profile.component';
