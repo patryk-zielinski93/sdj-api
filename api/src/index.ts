5a491702973eeb6b0098345d0e99894a07a1811e
Maciej Sikorski <maciej.sikorski@valueadd.pl>
2019-04-08 19:45:09 +0200

    API for starting and stopping ices containers

---
+++ api/src/index.ts
@@ -0,0 +1,47 @@
+import * as bodyParser from 'body-parser';
+import { spawn } from 'child_process';
+import * as express from 'express';
+import * as http from 'http';
+
+const app = express();
+const server = new http.Server(app);
+app.use(bodyParser.json());
+
+//TODO VALIDATE ID!!!
+app.get('/start/:id', function(req, res) {
+    const signal = spawn('bash', ['-c', `cd .. && docker-compose run -d --name slack_dj_ices_${req.params.id} -e ROOM_ID=${req.params.id} slack_dj_ices`]);
+
+    signal.stdout.on('data', data => {
+        console.log(`stdout: ${data}`);
+    });
+
+    signal.stderr.on('data', data => {
+        console.log(`stderr: ${data}`);
+    });
+
+    signal.on('close', code => {
+        console.log(`child process exited with code ${code}`);
+
+        code === 0 ? res.sendStatus(204) : res.sendStatus(500);
+    });
+});
+
+app.get('/remove/:id', function(req, res) {
+    const signal = spawn('bash', ['-c', `cd .. && docker rm -f slack_dj_ices_${req.params.id}`]);
+
+    signal.stdout.on('data', data => {
+        console.log(`stdout: ${data}`);
+    });
+
+    signal.stderr.on('data', data => {
+        console.log(`stderr: ${data}`);
+    });
+
+    signal.on('close', code => {
+        console.log(`child process exited with code ${code}`);
+
+        code === 0 ? res.sendStatus(204) : res.sendStatus(500);
+    });
+});
+
+server.listen(8887, () => console.log('Docker Api listening on 8888'));
