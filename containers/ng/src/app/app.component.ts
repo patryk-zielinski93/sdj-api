12b94903a56e047d1e2e620c24858ee4cdf0c12a
Maciej Sikorski <maciej.sikorski@infullmobile.com>
2019-07-09 09:21:53 +0200

    Moving list

--- containers/ng/src/app/app.component.ts
+++ containers/ng/src/app/app.component.ts
@@ -1,28 +1,37 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { environment } from '@environment/environment';
-import { Observable, Subject } from 'rxjs';
-import { filter, map, tap } from 'rxjs/operators';
-import { appConfig } from '../configs/app.config';
-import { QueuedTrack } from './common/interfaces/queued-track.interface';
-import { SpeechService } from './modules/core/services/speech.service';
-import { WebSocketService } from './modules/core/services/web-socket.service';
-import { AwesomePlayerComponent } from './modules/main/components/awesome-player/awesome-player.component';
+import { AfterViewInit, Component, ViewChild, ElementRef } from "@angular/core";
+import { environment } from "@environment/environment";
+import { Observable, Subject, zip } from "rxjs";
+import { filter, map, tap, delay, skipUntil } from "rxjs/operators";
+import { appConfig } from "../configs/app.config";
+import { QueuedTrack } from "./common/interfaces/queued-track.interface";
+import { SpeechService } from "./modules/core/services/speech.service";
+import { WebSocketService } from "./modules/core/services/web-socket.service";
+import { AwesomePlayerComponent } from "./modules/main/components/awesome-player/awesome-player.component";
+import { TrackUtil } from './modules/core/utils/track.util';
 
 @Component({
-  selector: 'sdj-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "sdj-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent implements AfterViewInit {
   audioSrc = environment.radioStreamUrl;
   currentTrack: Observable<any>;
-  queuedTracks$: Subject<QueuedTrack[]>;
-  @ViewChild('playerComponent')
+  getThumbnail = TrackUtil.getTrackThumbnail;
+  listScrollSubject: Subject<void> = new Subject();
+  queuedTracks: QueuedTrack[];
+  queuedTracks$: Observable<QueuedTrack[]>;
+  readonly queuedTrackskWidth = 210;
+  @ViewChild("playerComponent")
   playerComponent: AwesomePlayerComponent;
+  @ViewChild("toPlay")
+  toPlayContainer: ElementRef<HTMLElement>;
   prvTrackId: number;
 
-  constructor(private ws: WebSocketService, private speechService: SpeechService) {
-  }
+  constructor(
+    private ws: WebSocketService,
+    private speechService: SpeechService
+  ) {}
 
   ngAfterViewInit(): void {
     this.handleSpeeching();
@@ -30,55 +39,83 @@ export class AppComponent implements AfterViewInit {
   }
 
   handleQueuedTrackList(): void {
-    this.queuedTracks$ = this.ws.getQueuedTrackListSubject();
-    this.queuedTracks$.subscribe((list) => {
-      console.log(list);
-    });
-    this.queuedTracks$.next();
+    this.ws.getQueuedTrackListSubject().next();
+    this.queuedTracks$ = zip(
+      this.ws.getQueuedTrackListSubject().pipe(map(list => list.slice(1))),
+      this.listScrollSubject
+    ).pipe(
+      map(([list, scrolled]) => {
+        return list;
+      })
+    );
 
-    this.currentTrack = this.queuedTracks$
-      .pipe(map((list) => list[0]),
-        filter((track: any) => track && track.id !== this.prvTrackId),
-        tap((track: any) => this.prvTrackId = track.id)
-      );
+    this.ws
+      .getQueuedTrackListSubject()
+      .pipe(tap(list => this.handleScrollList(list.slice(1))))
+      .subscribe();
+
+    this.queuedTracks$.subscribe(list => {
+      this.queuedTracks = list;
+      this.toPlayContainer.nativeElement.scrollLeft += this.queuedTrackskWidth;
+    });
+
+    this.currentTrack = this.ws.getQueuedTrackListSubject().pipe(
+      map(list => list[0]),
+      filter((track: any) => track && track.id !== this.prvTrackId),
+      tap((track: any) => (this.prvTrackId = track.id))
+    );
   }
 
   handleSpeeching(): void {
     this.speechService.startListening();
-    this.speechService.speeching.subscribe(
-      (speeching: boolean) => {
-        if (speeching) {
-          this.playerComponent.player.audio.volume = 0.1;
-        } else {
-          this.playerComponent.player.audio.volume = 1;
-        }
+    this.speechService.speeching.subscribe((speeching: boolean) => {
+      if (speeching) {
+        this.playerComponent.player.audio.volume = 0.1;
+      } else {
+        this.playerComponent.player.audio.volume = 1;
       }
-    );
+    });
   }
 
   handleWsEvents(): void {
-    const connect$ = this.ws.createSubject('connect');
-    const events$ = this.ws.createSubject('events');
+    const connect$ = this.ws.createSubject("connect");
+    const events$ = this.ws.createSubject("events");
     connect$.subscribe(() => {
-      console.log('Connected');
-      events$.next(<any>{ test: 'test' });
+      console.log("Connected");
+      events$.next(<any>{ test: "test" });
       this.handleQueuedTrackList();
     });
-    events$.subscribe((data) => console.log('event', data));
-    const disconnect$ = this.ws.createSubject('disconnect');
-    disconnect$.subscribe(() => console.log('Disconnected'));
-    const exception$ = this.ws.createSubject('exception');
-    exception$.subscribe(() => console.log('Disconnected'));
+    events$.subscribe(data => console.log("event", data));
+    const disconnect$ = this.ws.createSubject("disconnect");
+    disconnect$.subscribe(() => console.log("Disconnected"));
+    const exception$ = this.ws.createSubject("exception");
+    exception$.subscribe(() => console.log("Disconnected"));
 
-    const playDJ$ = this.ws.createSubject('play_dj');
-    playDJ$.subscribe((data) => {
-      console.log('dj');
+    const playDJ$ = this.ws.createSubject("play_dj");
+    playDJ$.subscribe(data => {
+      console.log("dj");
       this.audioSrc = environment.radioStreamUrl;
     });
-    const playRadio$ = this.ws.createSubject('play_radio');
+    const playRadio$ = this.ws.createSubject("play_radio");
     playRadio$.subscribe(() => {
-      console.log('radio');
+      console.log("radio");
       this.audioSrc = appConfig.externalStream;
     });
+  }
+
+  handleScrollList(newList: QueuedTrack[]): void {
+    if (this.queuedTracks && newList.length < this.queuedTracks.length) {
+      let scrollAmount = 0;
+      const slideTimer: any = setInterval(() => {
+        this.toPlayContainer.nativeElement.scrollLeft -= 10;
+        scrollAmount += 10;
+        if (scrollAmount >= this.queuedTrackskWidth) {
+          this.listScrollSubject.next();
+          window.clearInterval(slideTimer);
+        }
+      }, 25);
+    } else {
+      this.listScrollSubject.next();
+    }
   }
 }
--- containers/ng/src/app/common/interfaces/track.interface.ts
+++ containers/ng/src/app/common/interfaces/track.interface.ts
@@ -1,3 +1,4 @@
 export interface Track {
+  id: string;
   title: string
 }
