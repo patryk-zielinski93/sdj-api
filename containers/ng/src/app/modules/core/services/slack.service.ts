54803ffe29cd3bb745dbe77f1c9a11e078383dda
Maciej Sikorski <maciej.sikorski@infullmobile.com>
2019-07-10 16:35:38 +0200

    Multiple redirect urls

--- containers/ng/src/app/modules/core/services/slack.service.ts
+++ containers/ng/src/app/modules/core/services/slack.service.ts
@@ -11,7 +11,7 @@ export class SlackService {
     }
 
     getSlackAuthorizationUrl(): string {
-        return `https://slack.com/oauth/authorize?scope=identity.basic&scope=channels:read,mpim:read,im:read&client_id=${environment.slack.clientId}&redirect_uri=&state=&user_scope=&granular_bot_scope=0&team=&install_redirect=&single_channel=0&tracked=1`;
+        return `https://slack.com/oauth/authorize?scope=identity.basic&scope=channels:read,mpim:read,im:read&client_id=${environment.slack.clientId}&redirect_uri=&state=&user_scope=&granular_bot_scope=0&team=&install_redirect=&single_channel=0&tracked=1&redirect_uri=${window.location.origin}`;
     }
 
     getAccessToken(code: string) {
@@ -19,7 +19,8 @@ export class SlackService {
             params: {
                 client_id: environment.slack.clientId,
                 client_secret: environment.slack.clientSecret,
-                code
+                code,
+                redirect_uri: window.location.origin
             }
         });
     }
