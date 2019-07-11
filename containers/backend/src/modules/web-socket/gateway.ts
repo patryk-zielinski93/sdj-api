109c2df6f8667c2b4be73590c0e483283074c130
Maciej Sikorski <maciej.sikorski@infullmobile.com>
2019-07-11 11:26:50 +0200

    Removing channel on site close

--- containers/backend/src/modules/web-socket/gateway.ts
+++ containers/backend/src/modules/web-socket/gateway.ts
@@ -1,55 +1,75 @@
-import { OnGatewayConnection, SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from '@nestjs/websockets';
-import { from, Observable, of } from 'rxjs';
-import { concatMap, delay, filter, switchMap } from 'rxjs/operators';
-import { Server, Socket } from 'socket.io';
+import {
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsResponse
+} from '@nestjs/websockets';
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Rooms, Server, Socket } from 'socket.io';
+
 import { QueuedTrack } from '../core/modules/db/entities/queued-track.entity';
 import { HostService } from '../core/services/host.service';
 import { WebSocketService } from '../core/services/web-socket.service';
 import { PlaylistStore } from '../core/store/playlist.store';
 
 @WebSocketGateway()
-export class Gateway implements OnGatewayConnection {
-    @WebSocketServer() server: Server;
+export class Gateway implements OnGatewayDisconnect {
+  private roomsSnapshot: Rooms;
+  @WebSocketServer() server: Server;
 
-    constructor(private service: WebSocketService, private readonly playlistStore: PlaylistStore) {
-    }
+  constructor(private service: WebSocketService, private readonly playlistStore: PlaylistStore) {}
 
-    handleConnection(client, ...args: any[]): any {
-    }
+  handleDisconnect(client, ...args: any[]): any {
+    this.leaveOtherChannels(client);
+  }
 
-    @SubscribeMessage('join')
-    async join(client: Socket, data: string): Promise<void> {
-        const room = JSON.parse(data).room;
-        if (!client.rooms[room]) {
-            if (!this.server.sockets.adapter.rooms[room]) {
-                HostService.startRadioStream(room)
-                    .then(() => {
-                        this.server.in(room)
-                            .emit('roomIsRunning');
-                    });
-            } else {
-                this.server.in(room)
-                    .emit('roomIsRunning');
-            }
-            const oldRoom = Object.keys(client.rooms).filter(item => item != client.id);
-            oldRoom.forEach((room) => {
-                client.leave(room);
-                if (!this.server.sockets.adapter.rooms[room]) {
-                    HostService.removeRadioStream(room);
-                }
-            });
-            client.join(room);
-            this.server.in(room)
-                .emit('newUser', 'New Player in ' + room);
-        }
-        return;
+  @SubscribeMessage('join')
+  async join(client: Socket, data: string): Promise<void> {
+    const room = JSON.parse(data).room;
+    if (!client.rooms[room]) {
+      this.joinRoom(client, room);
+      this.leaveOtherChannels(client, room);
+      this.server.in(room).emit('newUser', 'New Player in ' + room);
     }
+    return;
+  }
 
-    @SubscribeMessage('queuedTrackList')
-    onQueuedTrackList(client, data): Observable<WsResponse<QueuedTrack[]>> {
-        const queue = this.playlistStore.getQueue(JSON.parse(data));
-        return queue.pipe(switchMap(list => {
-            return of({ event: 'queuedTrackList', data: list });
-        }));
+  @SubscribeMessage('queuedTrackList')
+  onQueuedTrackList(client, data): Observable<WsResponse<QueuedTrack[]>> {
+    const queue = this.playlistStore.getQueue(JSON.parse(data));
+    return queue.pipe(
+      switchMap(list => {
+        return of({ event: 'queuedTrackList', data: list });
+      })
+    );
+  }
+
+  private doRoomsSnapshot(): void {
+    this.roomsSnapshot = {...this.server.sockets.adapter.rooms};
+  }
+
+  private joinRoom(client: Socket, room: string): void {
+    if (!this.server.sockets.adapter.rooms[room]) {
+      HostService.startRadioStream(room).then(() => {
+        this.server.in(room).emit('roomIsRunning');
+      });
+    } else {
+      this.server.in(room).emit('roomIsRunning');
     }
+    client.join(room);
+    this.doRoomsSnapshot();
+  }
+
+  private leaveOtherChannels(client: Socket, roomId?: string): void {
+    const otherRoomsKeys = Object.keys(this.roomsSnapshot).filter((key: string) => key !== roomId);
+    otherRoomsKeys.forEach(room => {
+      client.leave(room);
+      if (!this.server.sockets.adapter.rooms[room]) {
+        HostService.removeRadioStream(room);
+      }
+    });
+    this.doRoomsSnapshot();
+  }
 }
