b98de3a4008ade3b63ebd5f6a1539072f6d039f0
Maciej Sikorski <maciej.sikorski@infullmobile.com>
2019-07-15 12:42:18 +0200

    next api added channe

--- containers/backend/src/modules/api/controllers/app.controller.ts
+++ containers/backend/src/modules/api/controllers/app.controller.ts
@@ -1,3 +1,5 @@
+import { ChannelRepository } from './../../core/modules/db/repositories/channel.repository';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Controller, Get, Param, Render } from '@nestjs/common';
 import { Channel } from '../../core/modules/db/entities/channel.entity';
 import { HostService } from '../../core/services/host.service';
@@ -5,23 +7,24 @@ import { PlaylistService } from '../../core/services/playlist.service';
 
 @Controller()
 export class AppController {
-  constructor(private playlist: PlaylistService) {
-  }
+  constructor(
+    private playlist: PlaylistService,
+    @InjectRepository(ChannelRepository) private readonly channelRepository: ChannelRepository
+  ) {}
 
   @Get()
   @Render('index.hbs')
-  appView(): any {
-  }
+  appView(): any {}
 
   @Get('ices/:id')
   nexSong(@Param() params): any {
     HostService.nextSong(params.id);
   }
 
-  @Get('next')
-  removeNextSong(): any {
-      this.playlist.getNext({} as Channel)
-      .then(queuedTrack => {
+  @Get('next/:id')
+  async removeNextSong(@Param() params): Promise<any> {
+    const channel = await this.channelRepository.findOrCreate(params.id);
+    this.playlist.getNext(channel).then(queuedTrack => {
       if (queuedTrack) {
         this.playlist.removeQueuedTrack(queuedTrack);
         return `/tracks/${queuedTrack.track.id}.mp3`;
