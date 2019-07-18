2df25f906b331e7fdbb0e67f8edc034ad6dcd7d6
Maciej Sikorski <maciejsikorski00@gmail.com>
2019-07-18 10:27:27 +0200

    Added groups:read scope from slack

--- containers/ng/src/app/modules/core/services/slack.service.ts
+++ containers/ng/src/app/modules/core/services/slack.service.ts
@@ -11,7 +11,7 @@ export class SlackService {
     }
 
     getSlackAuthorizationUrl(): string {
-        return `https://slack.com/oauth/authorize?scope=identity.basic&scope=channels:read,mpim:read,im:read&client_id=${environment.slack.clientId}&redirect_uri=&state=&user_scope=&granular_bot_scope=0&team=&install_redirect=&single_channel=0&tracked=1&redirect_uri=${window.location.origin}`;
+        return `https://slack.com/oauth/authorize?scope=identity.basic&scope=groups:read,channels:read,mpim:read,im:read&client_id=${environment.slack.clientId}&redirect_uri=&state=&user_scope=&granular_bot_scope=0&team=&install_redirect=&single_channel=0&tracked=1&redirect_uri=${window.location.origin}`;
     }
 
     getAccessToken(code: string) {
