52ebdd0fb66f03fb0e61acc957ac5a7c965cedf3
Maciej Sikorski <maciejsikorski00@gmail.com>
2019-07-19 11:52:04 +0200

    Added debug option to nodemon builder

--- builders/nodemon/index.js
+++ builders/nodemon/index.js
@@ -4,22 +4,19 @@ var architect_1 = require("@angular-devkit/architect");
 var childProcess = require("child_process");
 exports["default"] = architect_1.createBuilder(function (options, context) {
     return new Promise(function (resolve) {
-        var target = architect_1.targetFromTargetString(options.buildTarget);
-        Promise.all([
-            context.getTargetOptions(target),
-            context.getBuilderNameForTarget(target)
-        ]).then(function (_a) {
-            var options = _a[0], builderName = _a[1];
-            var child = childProcess.spawn('nodemon', ['--config nodemon-debug.json']);
-            child.stdout.on('data', function (data) {
-                context.logger.info(data.toString());
-            });
-            child.stderr.on('data', function (data) {
-                context.logger.error(data.toString());
-            });
-            child.on('close', function (code) {
-                resolve({ success: true });
-            });
+        var args = [];
+        if (options.debug) {
+            args = args.concat(['--config', 'nodemon-debug.json']);
+        }
+        var child = childProcess.spawn('nodemon', args);
+        child.stdout.on('data', function (data) {
+            context.logger.info(data.toString());
+        });
+        child.stderr.on('data', function (data) {
+            context.logger.error(data.toString());
+        });
+        child.on('close', function (code) {
+            resolve({ success: true });
         });
     });
 });
--- builders/nodemon/index.ts
+++ builders/nodemon/index.ts
@@ -6,27 +6,21 @@ import {
 import * as childProcess from 'child_process';
 import { OptionsT } from './schema';
 
-export default createBuilder((options: any, context) => {
+export default createBuilder((options: OptionsT, context) => {
   return new Promise<BuilderOutput>(resolve => {
-    const target = targetFromTargetString(options.buildTarget);
-    Promise.all([
-      context.getTargetOptions(target),
-      context.getBuilderNameForTarget(target)
-    ]).then(([optionsT, builderName]: [OptionsT, string]) => {
-      const args = [];
-      if (optionsT.debug) {
-        args.concat(['--config', 'nodemon-debug.json']);
-      }
-      const child = childProcess.spawn('nodemon', args);
-      child.stdout.on('data', data => {
-        context.logger.info(data.toString());
-      });
-      child.stderr.on('data', data => {
-        context.logger.error(data.toString());
-      });
-      child.on('close', code => {
-        resolve({ success: true });
-      });
+    let args = [];
+    if (options.debug) {
+      args = args.concat(['--config', 'nodemon-debug.json']);
+    }
+    const child = childProcess.spawn('nodemon', args);
+    child.stdout.on('data', data => {
+      context.logger.info(data.toString());
+    });
+    child.stderr.on('data', data => {
+      context.logger.error(data.toString());
+    });
+    child.on('close', code => {
+      resolve({ success: true });
     });
   });
 });
