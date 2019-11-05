5e47134bbafae4ccb9818b3e673f41fa0cf6b02d
Maciej Sikorski <maciejsikorski00@gmail.com>
2019-11-05 09:17:20 +0100

    fix(ng): add missing fields in prod environment

--- apps/ng/src/environments/environment.prod.ts
+++ apps/ng/src/environments/environment.prod.ts
@@ -3,7 +3,12 @@ const browserWindowEnv = browserWindow['__env'] || {};
 
 export const environment: {
   backendUrl: string;
+  externalStream: string;
   radioStreamUrl: string;
+  slack: {
+    clientId: string;
+    clientSecret: string;
+  };
   production: boolean;
 } = {
   ...browserWindowEnv,
